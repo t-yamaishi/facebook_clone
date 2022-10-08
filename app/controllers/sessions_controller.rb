94d6915d3d82468d9387ba097ffe19499832bfec
yamaishi <yamakios8@icloud.com>
2022-10-08 22:15:30 +0900

    ログアウト機能実装

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,6 @@
 class SessionsController < ApplicationController
+  skip_before_action :login_required, only: [:new, :create]
+
   def new
   end
 
@@ -11,6 +13,12 @@ class SessionsController < ApplicationController
       flash.now[:danger] = 'ログインに失敗しました'
       render :new
     end
+  end
+
+  def destroy
+    session.delete(:user_id)
+    flash[:notice] = 'ログアウトしました'
+    redirect_to new_session_path
   end
 
 end
