a31d986f44a93cd6589c6fac483bfa2cd8db4551
yamaishi <yamakios8@icloud.com>
2022-10-09 13:21:45 +0900

    画像投稿機能を実装

--- Gemfile
+++ Gemfile
@@ -39,3 +39,6 @@ end
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 gem 'bcrypt'
+
+gem 'carrierwave'
+gem 'mini_magick'
--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,25 +1,20 @@
 class PicturesController < ApplicationController
   before_action :set_picture, only: %i[ show edit update destroy ]
 
-  # GET /pictures or /pictures.json
   def index
     @pictures = Picture.all
   end
 
-  # GET /pictures/1 or /pictures/1.json
   def show
   end
 
-  # GET /pictures/new
   def new
     @picture = Picture.new
   end
 
-  # GET /pictures/1/edit
   def edit
   end
 
-  # POST /pictures or /pictures.json
   def create
     @picture = current_user.pictures.build(picture_params)
     respond_to do |format|
@@ -33,7 +28,6 @@ class PicturesController < ApplicationController
     end
   end
 
-  # PATCH/PUT /pictures/1 or /pictures/1.json
   def update
     respond_to do |format|
       if @picture.update(picture_params)
@@ -46,7 +40,6 @@ class PicturesController < ApplicationController
     end
   end
 
-  # DELETE /pictures/1 or /pictures/1.json
   def destroy
     @picture.destroy
 
@@ -57,12 +50,10 @@ class PicturesController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_picture
       @picture = Picture.find(params[:id])
     end
 
-    # Only allow a list of trusted parameters through.
     def picture_params
       params.require(:picture).permit(:image, :content, :user_id)
     end
